23c599310bb00468fd1f57299d367c97c494cf63
gushteraa <141727561+gushteraa@users.noreply.github.com>
2023-08-09 22:48:09 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import time
+import requests
+from datetime import datetime
+import smtplib
+
+my_email = "your email"
+password = "your password"
+
+MY_LAT = 42.733883  # Your latitude
+MY_LONG = 25.485830  # Your longitude
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+data = response.json()
+
+iss_latitude = float(data["iss_position"]["latitude"])
+iss_longitude = float(data["iss_position"]["longitude"])
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+time_now = datetime.now()
+hour = time_now.hour
+
+while True:
+    time.sleep(6)
+    if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5:
+        if sunset <= hour <= sunrise:
+            with smtplib.SMTP("YOUR_SMTP_ADDRES") as connection:
+                connection.starttls()
+                connection.login(user=my_email, password=password)
+                connection.sendmail(from_addr=my_email,
+                                    to_addrs=my_email,
+                                    msg="Subject:Look Up\n\nlook up iss is above you!")
